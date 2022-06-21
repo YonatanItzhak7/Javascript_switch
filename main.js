0798b90331d6fa9e9dbc196deb9877615f25ad8c
Yonatanitzhak7 <Yonatan1171@gmail.com>
2022-06-21 18:40:41 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,102 @@
+// 1;
+// const btnColor = document.getElementById("btn_id");
+// btnColor.style.color = "green";
+// function printNumbers() {
+//   let userNum = +document.getElementById("num_id").value;
+//   switch (userNum) {
+//     case 3:
+//       console.log("Hello");
+//       break;
+//     case 5:
+//       console.log("Goodbye");
+//       break;
+//     case 7:
+//       console.log("Thank you");
+//       break;
+
+//     default:
+//       console.log("Number is not exist");
+//   }
+// }
+// printNumbers();
+// 2;
+// function printUserName() {
+//   let userText = document.getElementById("h2_id");
+//   let userName = document.getElementById("user_text").value;
+//   switch (userName) {
+//     case "jacob":
+//       userText.innerHTML = "Jacob";
+//       break;
+//     case "nathan":
+//       userText.innerHTML = userName.toUpperCase();
+//       break;
+//     case "DALYA":
+//       userText.innerHTML = userName.toLowerCase();
+//       break;
+//     default:
+//       userText.innerHTML = "The name is not exist";
+//       break;
+//   }
+// }
+// printUserName();
+// 3;
+// function printUserNumAndSigne() {
+//   switch (math_signe.value) {
+//     case "+":
+//       result_id.innerHTML = +num1_id.value + +num2_id.value;
+//       break;
+//     case "*":
+//       result_id.innerHTML = +num1_id.value * +num2_id.value;
+//       break;
+//     case "/":
+//       result_id.innerHTML = +num1_id.value / +num2_id.value;
+
+//       break;
+//     case "-":
+//       result_id.innerHTML = +num1_id.value - +num2_id.value;
+//       break;
+//   }
+// }
+
+// 4;
+// function printUserInput() {
+//   let text = document.getElementById("user_text");
+//   let color = document.getElementById("user_color");
+//   let number = document.getElementById("user_number");
+//   let myH2 = document.getElementById("h2_id")
+//   switch (text.value) {
+//     case text.value:
+//       myH2.innerHTML= text.value;
+//       myH2.style = "color:"+color.value+";"+"font-Size:"+number.value+"px;"
+//       break;
+//   }
+// }
+
+// // 5;
+// function startPostionElement() {
+//   switch (corner.value) {
+//     case "Top-Left":
+//       cicrcle.style =
+//         "border-radius:50%; width:50px; height:50px; background:" +
+//         colorInput.value;
+//       break;
+//     case "Top-Right":
+//       cicrcle.style =
+//         "border-radius:50%; width:50px; height:50px; position: fixed; left: 90vw; background:" +
+//         colorInput.value;
+//       break;
+//     case "Bottom-Left":
+//       cicrcle.style =
+//         "border-radius:50%; width:50px; height:50px; margin-top:15vw; background:" +
+//         colorInput.value;
+//       break;
+//     case "Bottom-Right":
+//       cicrcle.style =
+//         "border-radius:50%; width:50px; height:50px; margin-top:15vw; position:fixed; left:90vw; background:" +
+//         colorInput.value;
+//       break;
+//     default:
+//       break;
+//   }
+// }
+
